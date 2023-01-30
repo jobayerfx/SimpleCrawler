867b888cb3b00d5a262870de85fc4fa89f126189
yusuf <binnur@tikweb.dk>
2023-01-30 19:43:46 +0600

    Crawling Done Almost

--- app/Service/CrawlerService/CrawlerService.js
+++ app/Service/CrawlerService/CrawlerService.js
@@ -72,11 +72,6 @@ module.exports = {
         const htmlData = cheerio.load(pageHTML.data)
 
         const link = new URL(url).host
-        console.log('Host: ', link)
-
-        let image = null
-        let name = null
-        let price = null
 
         // console.log(htmlData('[data-testid="ProductDetailsBlockTestIds_name"]').text())
         // console.log("----------------PRICE-------------------")
@@ -89,57 +84,7 @@ module.exports = {
         // image = htmlData('[data-testid="grid:gallery:image:wrapper:0"]').find('img').attr('src')
         // console.log(image)
         // return res.status(200).json({name: name, price : price, images : image})
-        switch (link){
-            case 'www.ahlens.se':
-                name = htmlData('[data-testid="ProductDetailsBlockTestIds_name"]').text()
-                image = htmlData('.jss167 span > img').attr('src')
-                price = htmlData('[data-testid="ProductDetailsBlockTestIds_price"]').text()
-                break
-            case 'www.magasin.dk':
-                name = htmlData('.js-productName').text()
-                price = htmlData('.js-productPrice > .price > span > span > span ').text()
-                image = htmlData('.productDetailsImage__image').attr('src')
-                price = price.replace(/^\n|\n$/g, '')
-                break
-            case 'www.imerco.dk':
-                name = null
-                price = null
-                image = htmlData('.css-bjn8wh > div > div > img').attr('src')
-                break
-            case 'www.johnlewis.com':
-                name = htmlData('[data-testid="product:title"]').text()
-                price = htmlData('[data-testid="product:price"]').text()
-                image = htmlData('.Layout_image__1LfSG > div > div > div > div > img').attr('src') ?? null
-                break
-            case 'www.amazon.com':
-                name = htmlData('#productTitle').text()
-                const price_one = htmlData('.apexPriceToPay > span').first().text()
-                const price_two = htmlData('.priceToPay > span').first().text()
-                price = price_one ? price_one : price_two
-                image = htmlData('.imgTagWrapper > img').attr('src')
-                break
-            case 'www.elgiganten.dk':
-                name = htmlData('.product-title').text()
-                price = htmlData('.price__value > span').first().text().replace('.-','')
-                let pd_image = htmlData('.swiper-slide > img').attr('src')
-                image = pd_image ? pd_image : null
-                break
-            case 'www.georgjensen.com':
-                name = htmlData('.product-detail > div > h1').text().replace(/^\n|\n$/g, '')
-                price = htmlData('.product-price > span').first().text().replace('DKK','').replace(/^\n|\n$/g, '')
-                let p_image = htmlData('.product-image-carousel > a > picture > img').attr('src')
-                image = p_image ? "https://www.georgjensen.com" + p_image : null
-                break
-            case 'www.sinnerup.dk':
-                name = htmlData('.item-name  > h1').text().replace(/^\n|\n$/g, '')
-                price = htmlData('.item-prices__value').text()
-                let ps_image = htmlData('.item-prices__value > a').html()
-                image = ps_image ? ps_image : null
-                console.log(image)
-                break
-            default:
-                return res.status(200).json({name: null, price : null, images : null, link: link})
-        }
+
         // console.log(htmlData('div[class="jss319"]').text())
         // console.log("-----------------------------------")
         // console.log(htmlData('[data-testid="ProductDetailsBlockTestIds_price"]').html())
@@ -155,6 +100,63 @@ module.exports = {
         // console.log("----------------IMAGE-------------------")
         // image = htmlData('[data-testid="grid:gallery:image:wrapper:0"]').find('img').attr('src')
         // console.log(image)
-        return res.status(200).json({name: name, price : price, images : image, link: link})
+        let data = {name: null,  price : null, image : null, url: url, currency: null}
+        switch (link){
+            case 'www.ahlens.se':
+                data.name = htmlData('[data-testid="ProductDetailsBlockTestIds_name"]').text()
+                data.image = htmlData('.jss167 span > img').attr('src')
+                data.price = htmlData('[data-testid="ProductDetailsBlockTestIds_price"]').text()
+                break
+            case 'www.magasin.dk':
+                data.name = htmlData('.js-productName').text()
+                data.price = htmlData('.js-productPrice > .price > span > span > span ').text().replace(/^\n|\n$/g, '')
+                data.image = htmlData('.productDetailsImage__image').attr('src')
+                break
+            case 'www.johnlewis.com':
+                data.name = htmlData('[data-testid="product:title"]').text()
+                data.price = htmlData('[data-testid="product:price"]').text().replace('£','')
+                data.currency = 'EUR'
+                data.image = htmlData('.Layout_image__1LfSG > div > div > div > div > img').attr('src') ?? null
+                break
+            case 'www.amazon.com':
+                data.name = htmlData('#productTitle').text()
+                const price_one = htmlData('.apexPriceToPay > span').first().text()
+                const price_two = htmlData('.priceToPay > span').first().text()
+                data.price = price_one ? price_one : price_two
+                data.image = htmlData('.imgTagWrapper > img').attr('src')
+                break
+            case 'www.elgiganten.dk':
+                data.name = htmlData('.product-title').text()
+                data.price = htmlData('.price__value > span').first().text().replace('.-','')
+                let pd_image = htmlData('.swiper-slide > img').attr('src')
+                data.image = pd_image ? pd_image : null
+                break
+            case 'www.georgjensen.com':
+                data.name = htmlData('.product-detail > div > h1').text().toString().trim()
+                data.price = htmlData('.product-price > span').first().text().replace('DKK','').replace(/^\n|\n$/g, '')
+                let p_image = htmlData('.product-image-carousel > a > picture > img').attr('src')
+                data.image = p_image ? "https://www.georgjensen.com" + p_image : null
+                break
+            case 'www.sinnerup.dk':
+                data.name = htmlData('.item-name  > h1').text().toString().trim()
+                data.price = htmlData('.item-prices__value').text().replace('kr.','')
+                let ps_image = htmlData('.item-image > a').attr('data-srcset').split(',')[2].trim().replace('760w','')
+                data.image = ps_image ? ps_image : null
+                data.currency = 'Kr'
+                break
+            case 'www.imerco.dk':
+                data.name = htmlData('.ezs9ur40 > span').text().toString().trim()
+                data.price = htmlData('.e1vpke3j1').text().toString().trim()
+                let temp_image = htmlData('.css-bjn8wh > div > div > img').attr('src')
+                    .split('https://integration.imerco.dk/api').pop().trim()
+                data.image = 'https://integration.imerco.dk/api' + temp_image
+                break
+            default:
+                return res.status(200).json({error: false, message: 'Gift List', data})
+        }
+
+
+        return res.status(200).json({error: false, message: 'Gift List', data})
+
     }
 }
